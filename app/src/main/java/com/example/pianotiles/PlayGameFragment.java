2adf2d4c662576f991b48bd293034ac99a168d60
rajasacikal <75054325+rajasacikal@users.noreply.github.com>
2020-12-04 19:54:33 +0700

    Update Score & Lives

--- app/src/main/java/com/example/pianotiles/PlayGameFragment.java
+++ app/src/main/java/com/example/pianotiles/PlayGameFragment.java
@@ -5,7 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -15,7 +18,7 @@ import android.widget.TextView;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
-public class PlayGameFragment extends Fragment implements View.OnClickListener{
+public class PlayGameFragment extends Fragment implements View.OnClickListener, GestureDetector.OnGestureListener{
     public FragmentListener listener;
     protected Button btnStart;
     protected Bitmap mBitmap;
@@ -24,8 +27,10 @@ public class PlayGameFragment extends Fragment implements View.OnClickListener{
     protected UIThreadedWrapper uiThreadedWrapper;
     protected Tiles tiles;
     protected TilesThread tilesThread;
-    protected boolean canvasIsClean;
-    protected TextView tvScore;
+    protected boolean canvasIsClean,isStarted;
+    protected TextView tvScore,tvLives;
+    protected GestureDetector mDetector;
+    protected int scoreUpdate;
     public PlayGameFragment(){
 
     }
@@ -43,13 +48,20 @@ public class PlayGameFragment extends Fragment implements View.OnClickListener{
         View view = inflater.inflate(R.layout.fragment_play_game,container,false);
         this.btnStart = view.findViewById(R.id.btn_start);
         this.ivCanvas = view.findViewById(R.id.iv_canvas);
-
+        this.mDetector = new GestureDetector(this.getContext(),this);
         this.btnStart.setOnClickListener(this);
-
+        this.tvScore = view.findViewById(R.id.tv_score);
+        this.tvLives = view.findViewById(R.id.tv_lives);
         this.canvasIsClean = true;
-
+        this.isStarted = false;
+        this.scoreUpdate = 0;
         this.uiThreadedWrapper = new UIThreadedWrapper(this);
-
+        this.ivCanvas.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                return mDetector.onTouchEvent(motionEvent);
+            }
+        });
         return view;
     }
 
@@ -74,8 +86,9 @@ public class PlayGameFragment extends Fragment implements View.OnClickListener{
     }
 
     public void createFirstTiles(){
+        this.isStarted = true;
         this.tiles = new Tiles(0,0,this.ivCanvas.getWidth()/4,450,this.ivCanvas.getHeight());
-        this.tilesThread = new TilesThread(this.uiThreadedWrapper,this.tiles,this.ivCanvas.getWidth()/4);
+        this.tilesThread = new TilesThread(this.uiThreadedWrapper,this.tiles,this.ivCanvas.getWidth()/4,this.ivCanvas.getHeight());
         mBitmap = Bitmap.createBitmap(this.ivCanvas.getWidth(),this.ivCanvas.getHeight(),Bitmap.Config.ARGB_8888);
 
         this.ivCanvas.setImageBitmap(this.mBitmap);
@@ -92,6 +105,7 @@ public class PlayGameFragment extends Fragment implements View.OnClickListener{
     }
 
     public void createTiles(Tiles tiles){
+        this.tvLives.setText(Integer.toString(this.tiles.getLives()));
         this.deleteTiles();
         Paint paint = new Paint();
         int mColorTest = ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null);
@@ -108,5 +122,47 @@ public class PlayGameFragment extends Fragment implements View.OnClickListener{
         mCanvas.drawColor(mColorBackground);
 
         this.ivCanvas.invalidate();
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        int x = (int) e.getX();
+        int y = (int) e.getY();
+        int bot = this.tiles.getBottom();
+        int top = this.tiles.getTop();
+        int left = this.tiles.getLeft();
+        int right = this.tiles.getRight();
+        Log.d("touch_listener",x+"-"+y+"------"+top+"-"+bot+"-"+left+"-"+right);
+        if (this.isStarted && !this.tilesThread.getIsTilesClidked()){
+            if (this.tiles.getLeft()<=x && this.tiles.getRight()>=x && this.tiles.getTop()<=y && this.tiles.getBottom()>=y){
+                this.scoreUpdate++;
+                this.tvScore.setText(Integer.toString(scoreUpdate));
+                this.tilesThread.setTilesClicked(true);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
